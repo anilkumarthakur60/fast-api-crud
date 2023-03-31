3434573ef8518addb2e86658c537b9f436f5bdc2
Anil <anilkumarthakur60@gmail.com>
2023-04-01 01:39:07 +0545

    update

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# Fast-Crud
+
+# About FastApiCrud #
+It provides basic to advanced CRUD (Create, Read, Update, Delete) functionality for a given model.
+
+## Composer ##  
+## Installation ##
+```apacheconf
+composer require anil/fast-api-crud
+```
+* $model: The name of the model that this controller is responsible for.
+* $storeRequest: The name of the request class to use when storing data.
+* $updateRequest: The name of the request class to use when updating data.
+* $resource: The name of the resource class to use when returning data.
+
+
+### The class has several protected properties that can be overridden in child classes:###
+
+* ```$withAll```: An array of relationships to eager load when fetching all records.
+* ```withCount```: An array of relationships to count when fetching all records.
+* ```withAggregate```: An array of aggregate functions to apply when fetching all records.
+* ```loadAll```: An array of relationships to eager load when fetching a single record.
+* ```loadCount```: An array of relationships to count when fetching a single record.
+* ```loadAggregate```: An array of aggregate functions to apply when fetching a single record.
+* ```isApi```: A boolean indicating whether the controller is being used as an API or not.
+* ```forceDelete```: A boolean indicating whether to perform a soft delete or a hard delete.
+
+### The class has several methods that correspond to basic CRUD operations:###
+
+* ```index()```: Returns a paginated collection of all records.
+* ```store()```: Creates a new record.
+* ```update($id)```: Updates an existing record with the specified ID.
+* ```show($id)```: Returns a single record with the specified ID.
+* ```destroy($id)```: Deletes a single record with the specified ID.
+* ```delete()```: Deletes multiple records at once.
+
+#### There are also two helper methods, ```error()``` and ```success()```, that return a JSON response with a message and data. These are used to standardize error and success responses across the controller. ####
--- src/Controller/CrudBaseController.php
+++ src/Controller/CrudBaseController.php
@@ -6,13 +6,17 @@ use Exception;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Schema;
 
 class CrudBaseController extends Controller
 {
+    public array $scopes = [];
+    public array $scopeWithValue = [];
+    public array $loadScopes = [];
+    public array $loadScopeWithValue = [];
     protected array $withAll = [];
     protected array $withCount = [];
     protected array $withAggregate = [];
-
     protected array $loadAll = [];
     protected array $loadCount = [];
     protected array $loadAggregate = [];
@@ -20,12 +24,7 @@ class CrudBaseController extends Controller
     protected bool $forceDelete = FALSE;
 
 
-
-
-
-    public function __construct(public $model, public $storeRequest, public $updateRequest, public $resource)
-    {
-    }
+    public function __construct(public $model, public $storeRequest, public $updateRequest, public $resource) {}
 
 
     public function index()
@@ -40,6 +39,16 @@ class CrudBaseController extends Controller
             ->when(property_exists($this, 'withAggregate') && count($this->withAggregate), function ($query) {
                 foreach ($this->withAggregate as $key => $value) {
                     $query->withAggregate($key, $value);
+                }
+            })
+            ->when(property_exists($this, 'scopes') && count($this->scopes), function ($query) {
+                foreach ($this->scopes as $value) {
+                    $query->$value();
+                }
+            })
+            ->when(property_exists($this, 'scopeWithValue') && count($this->scopeWithValue), function ($query) {
+                foreach ($this->scopeWithValue as $key => $value) {
+                    $query->$key($value);
                 }
             });
 
@@ -69,28 +78,16 @@ class CrudBaseController extends Controller
     }
 
 
-
-    public function update($id)
+    public function error(
+        $message = 'Something went wrong',
+        $data = [],
+        $code = Response::HTTP_INTERNAL_SERVER_ERROR,
+    )
     {
-        $data = resolve($this->updateRequest)->safe()->only((new $this->model())->getFillable());
-
-        $model = $this->model::findOrFail($id);
-
-        try {
-            DB::beginTransaction();
-            $model->update($data);
-            if (method_exists(new $this->model(), 'afterUpdateProcess')) {
-                $model->afterUpdateProcess();
-            }
-
-            DB::commit();
-        } catch (Exception $e) {
-            DB::rollBack();
-
-            return $this->error($e->getMessage());
-        }
-
-        return $this->resource::make($model);
+        return response()->json([
+            'message' => $message,
+            'data' => $data,
+        ], $code);
     }
 
 
@@ -107,6 +104,16 @@ class CrudBaseController extends Controller
             ->when(property_exists($this, 'loadAggregate'), function ($query) {
                 foreach ($this->loadAggregate as $key => $value) {
                     $query->withAggregate($key, $value);
+                }
+            })
+            ->when(property_exists($this, 'loadScopes') && count($this->loadScopes), function ($query) {
+                foreach ($this->loadScopes as $value) {
+                    $query->$value();
+                }
+            })
+            ->when(property_exists($this, 'loadScopeWithValue') && count($this->loadScopeWithValue), function ($query) {
+                foreach ($this->loadScopeWithValue as $key => $value) {
+                    $query->$key($value);
                 }
             })
             ->findOrFail($id);
@@ -148,25 +155,86 @@ class CrudBaseController extends Controller
     }
 
 
-    public function error(
-        $message = 'Something went wrong',
+    public function success(
+        $message = 'Data fetched successfully',
         $data = [],
-        $code = Response::HTTP_INTERNAL_SERVER_ERROR,
-    ) {
+        $code = Response::HTTP_OK,
+    )
+    {
         return response()->json([
             'message' => $message,
             'data' => $data,
         ], $code);
     }
 
-    public function success(
-        $message = 'Data fetched successfully',
-        $data = [],
-        $code = Response::HTTP_OK,
-    ) {
-        return response()->json([
-            'message' => $message,
-            'data' => $data,
-        ], $code);
+
+    public function changeStatus($id)
+    {
+        $model = $this->model::findOrFail($id);
+        try {
+            DB::beginTransaction();
+            if (method_exists(new $this->model(), 'beforeChangeStatusProcess')) {
+                $model->beforeChangeStatusProcess();
+            }
+            if (!$this->checkFillable($model, ['status'])) {
+                DB::rollBack();
+                throw new Exception('Status column not found in fillable');
+            }
+            $model->update(['status' => $model->status === 1 ? 0 : 1]);
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+
+            return $this->error($e->getMessage());
+        }
+
+        return $this->resource::make($model);
     }
+
+
+    private function checkFillable($model, $columns): bool
+    {
+        $fillableColumns = $this->fillableColumn($model);
+
+        $diff = array_diff($columns, $fillableColumns);
+
+        return count($diff) > 0 ? FALSE : TRUE;
+    }
+
+
+    public function update($id)
+    {
+        $data = resolve($this->updateRequest)->safe()->only((new $this->model())->getFillable());
+
+        $model = $this->model::findOrFail($id);
+
+        try {
+            DB::beginTransaction();
+            $model->update($data);
+            if (method_exists(new $this->model(), 'afterUpdateProcess')) {
+                $model->afterUpdateProcess();
+            }
+
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+
+            return $this->error($e->getMessage());
+        }
+
+        return $this->resource::make($model);
+    }
+
+
+    private function fillableColumn($model): array
+    {
+        return Schema::getColumnListing($this->tableName($model));
+    }
+
+
+    private function tableName($model): string
+    {
+        return $model->getTable();
+    }
+
 }
