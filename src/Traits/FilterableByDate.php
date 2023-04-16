1d72205078878f4c06657989cc8d06bcfa79578c
Anil <anilkumarthakur60@gmail.com>
2023-04-16 08:22:22 +0545

    Few scope Added

---
+++ src/Traits/FilterableByDate.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Anil\FastApiCrud\Traits;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
+
+trait FilterableByDates
+{
+    public function scopeToday(Builder $query, $column = 'created_at'): Builder
+    {
+        return $query->whereDate($column, Carbon::today());
+    }
+
+    public function scopeYesterday(Builder $query, $column = 'created_at'): Builder
+    {
+        return $query->whereDate($column, Carbon::yesterday());
+    }
+
+    public function scopeMonthToDate(Builder $query, $column = 'created_at'): Builder
+    {
+        return $query->whereBetween($column, [Carbon::now()->startOfMonth(), Carbon::now()]);
+    }
+
+    public function scopeQuarterToDate(Builder $query, $column = 'created_at'): Builder
+    {
+        $now = Carbon::now();
+        return $query->whereBetween($column, [$now->startOfQuarter(), $now]);
+    }
+
+    public function scopeYearToDate(Builder $query, $column = 'created_at'): Builder
+    {
+        return $query->whereBetween($column, [Carbon::now()->startOfYear(), Carbon::now()]);
+    }
+
+    public function scopeLast7Days(Builder $query, $column = 'created_at'): Builder
+    {
+        return $query->whereBetween($column, [Carbon::today()->subDays(6), Carbon::now()]);
+    }
+
+    public function scopeLast30Days(Builder $query, $column = 'created_at'): Builder
+    {
+        return $query->whereBetween($column, [Carbon::today()->subDays(29), Carbon::now()]);
+    }
+
+    public function scopeLastQuarter(Builder $query, $column = 'created_at'): Builder
+    {
+        $now = Carbon::now();
+        return $query->whereBetween($column, [$now->startOfQuarter()->subMonths(3), $now->startOfQuarter()]);
+    }
+
+    public function scopeLastYear(Builder $query, $column = 'created_at'): Builder
+    {
+        return $query->whereBetween($column, [Carbon::now()->subYear(), Carbon::now()]);
+    }
+}
